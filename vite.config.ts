ab522c37e815befd3daf0f6f59ba144a7bf280d0
kanishkakandula <126744988+kanishkakandula@users.noreply.github.com>
2026-09-21 13:52:49 +0800

    Add files via upload
    Change the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Plan3D/',
   build: {
     target: 'es2020',
     chunkSizeWarningLimit: 1600,
